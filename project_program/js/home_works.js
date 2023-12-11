53064c3e3a4c8868218b0ad8b7ceb6c2bc19e968
Baytana <151927837+Baytana@users.noreply.github.com>
2023-12-12 00:09:33 +0600

    HomeWork-2

--- project_program/js/home_works.js
+++ project_program/js/home_works.js
@@ -1,3 +1,4 @@
+//MOVE BLOCK
 function validateEmail(){
     const emailInput=document.getElementById('gmail_input')
     const result = document.getElementById('gmail_result');
@@ -19,20 +20,75 @@ function validateEmail(){
     }
 }
 
-//движения маленького блока
-function moveBlock(position,direction){
-    const smallBlock=document.querySelector('.child_block')
-    smallBlock.style.left=position+'px'
 
-    if (position<450 && direction==='right'){
-        setTimeout(()=> moveBlock(position+1,'right'),10)
-    }else if(position>0 && direction==='left'){
-        setTimeout(()=>moveBlock(position-1,'left'),10)
-    }else{
-        moveBlock(position,direction==='right'?'left':'right')
+//движения маленького блока
+const childBlock=document.querySelector('.child_block')
+let parentFReeWidth=450
+let moveSpeedChildBlock=5
+let positionX=0
+let positionY=0
+
+const moveBlock=()=>{
+    if(positionX<=parentFReeWidth && positionY===0){
+        childBlock.style.left=`${positionX++}px`
+        setTimeout(moveBlock,moveSpeedChildBlock)
+    }
+    else if (positionX>=parentFReeWidth && positionY<parentFReeWidth){
+        childBlock.style.top=`${positionY++}px`
+        setTimeout(moveBlock,moveSpeedChildBlock)
+    }
+    else if(positionX>=0 && positionY===parentFReeWidth){
+        positionX--
+        childBlock.style.left=`${positionX}px`
+        setTimeout(moveBlock,moveSpeedChildBlock)
+    }
+    else if (positionX<parentFReeWidth && positionY<=parentFReeWidth){
+        childBlock.style.top=`${positionY--}px`
+        setTimeout(moveBlock,moveSpeedChildBlock)
     }
 }
+moveBlock()
 
-window.onload=function (){
-    moveBlock(0,'right')
+
+//STOPWATCH
+
+let timerId
+let time=0
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+
+const stopwatch=()=>{
+    // time++
+    const minute=Math.floor(time/60)
+    const second=time%60
+    time++
+    const resultTime=`${padZero(minute)}:${padZero(second)}`
+    document.getElementById('secondsS').innerText=resultTime
+
+}
+const start=()=>{
+    timerId=setInterval(stopwatch,1000)
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = false;
+}
+const stop=()=>{
+    clearInterval(timerId)
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = false;
+}
+
+const reset=()=>{
+    stop()
+    time=0
+    stopwatch()
+    startButton.disabled = false;
+    stopButton.disabled = false;
+    resetButton.disabled = true;
+}
+
+const padZero=(number)=>{
+    return number<10 ? `0${number}` : `${number}`
 }
